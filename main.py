305f32d42b0cda3d6d6626e12a0cf1a85b8e72c2
Yummy Bian <yummy.bian@gmail.com>
2018-03-22 12:10:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
     bc.add_block("Send 1 BTC to Ivan")
     bc.add_block("Send 2 more BTC to Ivan")
 
-    blocks_it = bc.blocks
-    for block in blocks_it:
+    for block in bc.blocks:
         print("Prev. hash: {0}".format(block.prev_block_hash))
         print("Data: {0}".format(block.data))
         print("Hash: {0}".format(block.hash))
